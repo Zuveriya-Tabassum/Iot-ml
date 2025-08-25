44e42beedc94bd6ccd0a55342cf09c260641369f
Zuveriya-Tabassum <tabassumzuveriya@gmail.com>
2025-08-25 16:15:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
 from flask import Flask,request
 from main import generateAI
-import pickle
+import pickle 
+
 generateAI()
-ai=pickle.load(open('model.pkl','rb')) #unpickling
+ai=pickle.load(open('model.pkl','rb'))
 
-app=Flask(__name__)
+app=Flask(_name_)
 
-#for APIs we use decorater @,and decorater function(def ) should follow immediately after decorater(@)
-@app.route('/')
+@app.route('/') 
 def home():
-    return('AI model server is running')
+    return('AI Model Server is Running')
 
-#main API
-@app.route('/predict',methods=['GET']) # '/predict':-decorater name 
+@app.route('/predict',methods=['GET'])
 def predict():
-    temp=request.args.get('temp') #temp :input value,request-IoT takes through requests
-    temp=float(temp)              #input always return str ,convert to float
-    data=[[temp]]                 #preparing data i.e. in 2D array [[]]
-    result=ai.predict(data)       #ai predicts an array
-    #api always have string format as return type
-    result=result[0]              #result[0] is str:on/off
+    temp=request.args.get('temp')
+    temp=float(temp)
+    data=[[temp]]
+    result=ai.predict(data)
+    result=result[0]
     return (result)
 
-#starting the server
-if(__name__=="__main__"):
-    #host:IPv4 i.e.a.b.c.d,0.0.0.0 means take respective websites/machine's IP address
-    app.run(host='0.0.0.0',port=5000,debug=True) 
-
-
-
-
-
+if(_name=="main_"):
+    app.run(host='0.0.0.0',port=5000,debug=True)
