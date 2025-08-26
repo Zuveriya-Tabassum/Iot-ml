415035d716e31a0252a3cdf683a3b5cbad7bebd3
Zuveriya-Tabassum <tabassumzuveriya@gmail.com>
2025-08-26 10:16:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
+# import pandas as pd
+
+# def generateAI():
+#     dataset=pd.read_csv('ml-data.csv')
+#     dataset=dataset.dropna()                       #delete NaN rows              
+#     X=dataset.iloc[:,1].values                     #sepearte X values(i.e. temp) from dataset
+#     X=X.reshape(-1,1)                              #converts 1D array to 2D array
+#     y=dataset.iloc[:,-1].values                    #load y values
+#     from sklearn.preprocessing import LabelEncoder # Encode the target variable
+#     le = LabelEncoder()
+#     y = le.fit_transform(y)
+
+#     from sklearn.model_selection import train_test_split  #split the data
+#     X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+
+#     from sklearn.neighbors import KNeighborsRegressor  # applying k nearest neighbors (utility distance)
+#     ai=KNeighborsRegressor(n_neighbors=5)         #here no. of neighbours=5
+#     ai.fit(X_train,y_train)
+#     import pickle
+
+#     pickle.dump(ai,open('model.pkl','wb'))
 import pandas as pd
 
 def generateAI():
-    dataset=pd.read_csv('ml-data.csv')
-    dataset=dataset.dropna()                       #delete NaN rows              
-    X=dataset.iloc[:,1].values                     #sepearte X values(i.e. temp) from dataset
-    X=X.reshape(-1,1)                              #converts 1D array to 2D array
-    y=dataset.iloc[:,-1].values                    #load y values
-    from sklearn.preprocessing import LabelEncoder # Encode the target variable
-    le = LabelEncoder()
-    y = le.fit_transform(y)
+    dataset = pd.read_csv('data.csv')
+    dataset = dataset.dropna()
+    X= dataset.iloc[:,1].values
+    X= X.reshape(-1,1)
+    y=dataset.iloc[:,-1].values
 
-    from sklearn.model_selection import train_test_split  #split the data
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+    from sklearn.model_selection import train_test_split
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
 
-    from sklearn.neighbors import KNeighborsRegressor  # applying k nearest neighbors (utility distance)
-    ai=KNeighborsRegressor(n_neighbors=5)         #here no. of neighbours=5
+    from sklearn.neighbors import KNeighborsClassifier
+    ai = KNeighborsClassifier(n_neighbors=5)
+
     ai.fit(X_train,y_train)
-    import pickle
+
+    import pickle 
     pickle.dump(ai,open('model.pkl','wb'))
